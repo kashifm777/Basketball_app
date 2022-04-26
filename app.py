f945ef00c872059970b499ea2fd09ff934315304
Muhammad Kashif <muhammadkashif841@gmail.com>
2022-04-26 11:34:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,7 @@ st.header('Display Player Stats of Selected Team(s)')
 st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
 st.dataframe(df_selected_team)
 
-# Download NBA player stats data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
